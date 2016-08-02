b3e8abdab56a82397428a633fe8a5eed6744cc92
Manvydas <manwiss@gmail.com>
2016-08-02 16:51:43 +0300

    Added Enum( which is used in DataManager.GetMetric method, Completed "FullDataManager" Class, Added more cases for DataManager.GetMetric method

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,15 +12,64 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            /// console output doesnt matter, DATAMANAGER could return the summary, could return the hardware and application list, if i can get those - so good!
             var dataManager = new FullDataManager();
 
-            var computername = dataManager.GetMetric("computername");
+            var computername = dataManager.GetMetric(ComputerMetrics.ComputerName);
             Console.WriteLine($"Computer Name: {computername}");
 
-            var cpuUsage = dataManager.GetMetric("cpuusage");
+            var userName = dataManager.GetMetric(ComputerMetrics.User);
+            Console.WriteLine($"Current user name: {userName}");
+
+            var cpuUsage = dataManager.GetMetric(ComputerMetrics.CpuUsage);
             Console.WriteLine($"Current CPU usage: {cpuUsage}%");
-            Console.ReadLine();
+
+            var AmountOfRam = dataManager.GetMetric(ComputerMetrics.Ram);
+            Console.WriteLine($"Total amount of ram: {AmountOfRam}kb");
+
+            var NameOfVideoCard = dataManager.GetMetric(ComputerMetrics.VideoCard);
+            Console.WriteLine($"Name of video card: {NameOfVideoCard}");
+
+            var IP = dataManager.GetMetric(ComputerMetrics.Ip);
+            Console.WriteLine($"Curent IP adress: {IP}");
+
+            var RamPercentage = dataManager.GetMetric(ComputerMetrics.RamUsage);
+            Console.WriteLine($"Total amount of ram used: {RamPercentage}%");
+
+            var NameOfCpu = dataManager.GetMetric(ComputerMetrics.CpuName);
+            Console.WriteLine($"Name of CPU: {NameOfCpu}");
+
+            var availableDiskSpace = dataManager.GetMetric(ComputerMetrics.AvailableDiskSpace);
+            Console.WriteLine($"Available disk disk space: {availableDiskSpace}MB");
+
+
+            var DiskQueueLenght = dataManager.GetMetric(ComputerMetrics.AverageDiskQueueLength);
+            Console.WriteLine($"Average disk queue lenght: {DiskQueueLenght}");
+
+            List<string> applicationList = dataManager.GetApplicationList();
+
+            Console.WriteLine();
+            Console.WriteLine("List of applications installed");
+            foreach (string item in applicationList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("List of hardware");
+            
+            List<string> HardwareList = dataManager.GetHardwareList();
+            foreach (var Hardware in HardwareList)
+            {
+                Console.WriteLine(Hardware);
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
--- ConsoleApplication1/DataLayer/FullDataManager.cs
+++ ConsoleApplication1/DataLayer/FullDataManager.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Management;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace DataLayer
 {
@@ -10,17 +14,53 @@ namespace DataLayer
     {
         public override ComputerSummary GetComputerSummary()
         {
-            throw new NotImplementedException();
+            ComputerSummary computerSummary = new ComputerSummary();
+            computerSummary.Name = GetMetric(ComputerMetrics.ComputerName);
+            computerSummary.User= GetMetric(ComputerMetrics.User);
+            computerSummary.Cpu= GetMetric(ComputerMetrics.CpuName);
+            computerSummary.Ram= Convert.ToInt32(GetMetric(ComputerMetrics.Ram));
+            computerSummary.VideoCard= GetMetric(ComputerMetrics.VideoCard);
+            computerSummary.Ip= new IPAddress(Convert.ToInt64(GetMetric(ComputerMetrics.Ip)));
+            computerSummary.CpuUsage= Convert.ToInt32(GetMetric(ComputerMetrics.CpuUsage));
+            computerSummary.RamUsage= Convert.ToInt32(GetMetric(ComputerMetrics.RamUsage));
+            computerSummary.AvailableDiskSpaceGb= Convert.ToInt32(GetMetric(ComputerMetrics.AvailableDiskSpace));
+            computerSummary.AverageDiskQueueLength= Convert.ToInt32(GetMetric(ComputerMetrics.AverageDiskQueueLength));
+
+
+            return computerSummary;
+
         }
 
         public override List<string> GetApplicationList()
         {
-            throw new NotImplementedException();
+
+            List<string> applicationList = new List<string>();
+            ManagementObjectSearcher ManagementObjectSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
+            foreach (ManagementObject managementObject in ManagementObjectSearcher.Get())
+            {
+               applicationList.Add(managementObject["Name"].ToString());
+            }
+            
+            return applicationList;
         }
 
         public override List<string> GetHardwareList()
         {
-            throw new NotImplementedException();
+           List<string> HardwareList = new List<string>();
+
+
+            ManagementObjectSearcher magaManagementObjectSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity");
+            foreach (ManagementObject managementObject in magaManagementObjectSearcher.Get())
+            {
+              if(managementObject["Name"] != null)
+                    HardwareList.Add(managementObject["Name"].ToString());
+            }
+
+            return HardwareList;
+
+
+            }
+     
         }
     }
-}
+
