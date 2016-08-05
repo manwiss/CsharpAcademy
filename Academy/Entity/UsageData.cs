c21a0e53ce96e6a7ecec363a230a685568992d79
Manvydas <manwiss@gmail.com>
2016-08-05 15:31:04 +0300

    Implemented Entity Framework

---
+++ Academy/Entity/UsageData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class UsageData
+    {
+        public DateTime? Time { get; set; }
+        public int CpuUsage { get; set; }
+        public int RamUsage { get; set; }
+        public int AvailableDiskSpaceGb { get; set; }
+        public int AverageDiskQueueLength { get; set; }
+        public int UsageDataId { get; set; }
+
+    }
+}
